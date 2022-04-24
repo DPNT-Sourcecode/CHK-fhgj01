441815c2808399b9a8697819401e323e915d99ca
sbx_user1051 <sbx_user1051@169.254.155.253>
2022-04-24 14:17:48 +0000

    Sun Apr 24 14:17:48 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,43 @@
-from lib.solutions.CHK.data import product_data
+#from lib.solutions.CHK.data import product_data
+
+product_data = {
+    "A": {
+        "price": 50,
+        "offer": {
+            "is_offer": True,
+            "quantity": 3,
+            "value": 130
+        }
+    },
+    "B": {
+        "price": 30,
+        "offer": {
+            "is_offer": True,
+            "quantity": 2,
+            "value": 45
+        }
+    },
+    "C": {
+        "price": 20,
+        "offer": {
+            "is_offer": False,
+            "quantity": 0,
+            "value": 0
+        }
+    },
+    "D": {
+        "price": 15,
+        "offer": {
+            "is_offer": False,
+            "quantity": 0,
+            "value": 0
+        }
+    },
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     for item in skus:
-        if item not in product_data.keys():
+        if item not in list(product_data.keys()):
             return -1
