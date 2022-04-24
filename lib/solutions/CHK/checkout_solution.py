a4a4db565238baa4727339097e52c2ad4b8ee03b
sbx_user1051 <sbx_user1051@169.254.155.253>
2022-04-24 14:05:48 +0000

    Sun Apr 24 14:05:48 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,8 @@
-
+from data import product_data
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    for item in skus:
+        if item not in product_data.keys():
+            return -1
