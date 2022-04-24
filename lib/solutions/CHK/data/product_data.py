e0b7d9b16e4bb4abae2c0abf6bd95e190b1211fd
sbx_user1051 <sbx_user1051@169.254.155.253>
2022-04-24 13:59:48 +0000

    Sun Apr 24 13:59:48 UTC 2022

---
+++ lib/solutions/CHK/data/product_data.py
@@ -0,0 +1,34 @@
+product_data = {
+    "A": {
+        "price": 50,
+        "offer": {
+            "is_offer": True,
+            "quantity": 3,
+            "value": 130
+        }
+    },
+    "B": {
+        "price": 30,
+        "offer": {
+            "is_offer": True,
+            "quantity": 2,
+            "value": 45
+        }
+    },
+    "C": {
+        "price": 20,
+        "offer": {
+            "is_offer": False,
+            "quantity": 0,
+            "value": 0
+        }
+    },
+    "D": {
+        "price": 15,
+        "offer": {
+            "is_offer": False,
+            "quantity": 0,
+            "value": 0
+        }
+    },
+}
